d40834834dbaa355214af8d807940f06cfae99fe
Lucas K. <101528422+lucaskotowski@users.noreply.github.com>
2025-07-11 01:35:19 -0300

    Update create_bronze_tables.sql
    updated script

--- sql-projects/datawarehouse-project/scripts/create_bronze_tables.sql
+++ sql-projects/datawarehouse-project/scripts/create_bronze_tables.sql
@@ -1,4 +1,4 @@
--- Script para criação das tabelas utilizadas na camada bronze
+-- Esse script cria as tabelas dentro do schema bronze, trazendo os dados as-is da fonte
 
 CREATE TABLE bronze.crm_cust_info (
 	cst_id int,
@@ -8,4 +8,44 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_marital_status VARCHAR(50),
 	cst_gndr VARCHAR(50),
 	cst_create_date DATE
+);
+
+CREATE TABLE bronze.crm_prd_info (
+	prd_id int,
+	prd_key VARCHAR(50),
+	prd_name VARCHAR(50),
+	prd_cost int,
+	prd_line VARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE
+);
+
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id int,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales int,
+	sls_quantity int,
+	sls_price int
+);
+
+CREATE TABLE bronze.erp_CUST_AZ12 (
+	CID VARCHAR(50),
+	BDATE DATE,
+	GEB VARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_LOC_A101 (
+	CID VARCHAR(50),
+	CNTRY VARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_PX_CAT_G1V2 (
+	ID VARCHAR(50),
+	CAT VARCHAR(50),
+	SUBCAT VARCHAR(50),
+	MAINTENANCE VARCHAR(50)
 );
