4518914347c30afedc518a4228dbd98fac89e9c4
Lucas K. <101528422+lucaskotowski@users.noreply.github.com>
2025-08-01 10:13:01 -0300

    Update create_silver_tables.sql
    updated silver script

--- sql-projects/datawarehouse-project/scripts/create_silver_tables.sql
+++ sql-projects/datawarehouse-project/scripts/create_silver_tables.sql
@@ -13,6 +13,7 @@ CREATE TABLE silver.crm_cust_info (
 DROP TABLE IF EXISTS silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
 	prd_id int,
+	cat_id VARCHAR(50),
 	prd_key VARCHAR(50),
 	prd_name VARCHAR(50),
 	prd_cost int,
