bb03e52960f93de2818f81073ab9b9a5128fc014
Lucas K. <101528422+lucaskotowski@users.noreply.github.com>
2025-08-05 11:24:07 -0300

    Update load_silver_data.sql
    added crm_sales_details script

--- sql-projects/datawarehouse-project/scripts/load_silver_data.sql
+++ sql-projects/datawarehouse-project/scripts/load_silver_data.sql
@@ -45,3 +45,46 @@ END AS prd_line,
 CAST(prd_start_dt AS DATE) AS prd_start_dt,
   (LEAD(CAST(prd_start_dt AS DATE)) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day') AS prd_end_dt
 FROM bronze.crm_prd_info;
+
+
+-- silver.crm_sales_details
+
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+SELECT 
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE 
+		WHEN sls_order_dt = 0 OR LENGTH(TRIM(sls_order_dt::text)) != 8 THEN NULL
+		ELSE TO_DATE(sls_order_dt::text, 'YYYYMMDD')
+	END AS sls_order_dt,
+	CASE 
+		WHEN sls_ship_dt = 0 OR LENGTH(TRIM(sls_ship_dt::text)) != 8 THEN NULL
+		ELSE TO_DATE(sls_ship_dt::text, 'YYYYMMDD')
+	END AS sls_ship_dt,
+	CASE 
+		WHEN sls_due_dt = 0 OR LENGTH(TRIM(sls_due_dt::text)) != 8 THEN NULL
+		ELSE TO_DATE(sls_due_dt::text, 'YYYYMMDD')
+	END AS sls_due_dt,
+	CASE 
+		WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+			THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE 
+		WHEN sls_price IS NULL OR sls_price <= 0 
+			THEN sls_sales / NULLIF(sls_quantity, 0)
+		ELSE sls_price
+	END AS sls_price
+FROM bronze.crm_sales_details;
