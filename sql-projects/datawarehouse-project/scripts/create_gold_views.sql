11523981848bfad8685bf20f1ff7da048a6fab05
Lucas K. <101528422+lucaskotowski@users.noreply.github.com>
2025-08-06 18:48:35 -0300

    Update create_gold_views.sql
    added dim_products

--- sql-projects/datawarehouse-project/scripts/create_gold_views.sql
+++ sql-projects/datawarehouse-project/scripts/create_gold_views.sql
@@ -19,3 +19,23 @@ SELECT
 FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid;
+
+
+--gold.dim_products
+
+CREATE VIEW gold.dim_products AS
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY prd_start_dt, prd_key) AS product_key,
+	prd_id AS product_number,
+	prd_key AS product_id,
+	prd_name AS product_name,
+	cat_id AS categorie_id,
+	cat AS categorie,
+	subcat AS subcategorie,
+	maintenance,
+	prd_cost AS product_cost,
+	prd_line AS product_line,
+	prd_start_dt AS product_start_date
+FROM silver.crm_prd_info pi
+LEFT JOIN silver.erp_px_cat_g1v2 pc ON pi.cat_id = pc.id
+WHERE prd_end_dt IS NULL
